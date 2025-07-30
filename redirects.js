0fe36125b221a28d5ee67adbb09bb9ef364b0916
Tiendas3B <soporte@tiendas3b.com.bo>
2025-07-30 16:27:48 -0400

    update 3001

--- redirects.js
+++ redirects.js
@@ -1,5 +1,6 @@
 // redirects.js
 const redirects = {
+	"/3001": "https://chat.whatsapp.com/KG1m7yiPVCJ3q7lPxQTC6D",
   "/enlace1": "https://www.google.com",
   "/enlace2": "https://www.tiendas3b.com.bo",
   "/linkedin": "https://linkedin.com/in/tuusuario",
