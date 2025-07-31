6ed2a9f9c84720b280b6c531d5e0cc816fc5be35
Tiendas3B <soporte@tiendas3b.com.bo>
2025-07-31 14:18:37 -0400

    Prueba que quiere andres

--- redirects.js
+++ redirects.js
@@ -1,6 +1,6 @@
 // redirects.js
 const redirects = {
-	"/3001": "https://chat.whatsapp.com/KG1m7yiPVCJ3q7lPxQTC6D",
+	"/3001": "https://www.tiktok.com/@tiendas3b_bolivia?_t=ZM-8yUe8YnkJUL&_r=1",
   "/enlace1": "https://www.google.com",
   "/enlace2": "https://www.tiendas3b.com.bo",
   "/linkedin": "https://linkedin.com/in/tuusuario",
