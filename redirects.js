a8aaa44116c911d98896dd3c95a782cb30c5c1ca
Tiendas3B <soporte@tiendas3b.com.bo>
2025-11-01 18:55:09 -0400

    Add files via upload

--- redirects.js
+++ redirects.js
@@ -55,7 +55,7 @@ const redirects = {
   "/whatsappclubvecinos": "https://chat.whatsapp.com/JruTMAwYrBt5bOJLNpr5PQ",
   "/whatsappfexpo": "https://chat.whatsapp.com/JruTMAwYrBt5bOJLNpr5PQ?mode=wwt",
   "/tiendas3b": "https://www.tiendas3b.com.bo",
-  "/pauta": "https://chat.whatsapp.com/HyGRRWOrkNaLRKec15J3zr",
+  "/pauta": "https://chat.whatsapp.com/JruTMAwYrBt5bOJLNpr5PQ",
   "/masivo": "https://chat.whatsapp.com/JruTMAwYrBt5bOJLNpr5PQ",
   "/bpo": "https://tiendas3b.evaluar.com/evjm/606335ee-9fe9-4ae7-b6ad-50f16edb3ede"
 };
